63d819af0ce9a4452ef58e833d4755eaf8114c84
Crystal Adkins <CrystalAdkins09@gmail.com>
2024-06-06 15:43:04 -0400

    Add turnkey passkey client

--- src/lib/_turnkey/turnkeyClient.ts
+++ src/lib/_turnkey/turnkeyClient.ts
@@ -1,10 +1,18 @@
 import {Turnkey} from '@turnkey/sdk-server';
+import {Turnkey as PasskeyClient} from '@turnkey/sdk-browser';
 
-const turnkey = new Turnkey({
-  apiBaseUrl: process.env.NEXT_PUBLIC_TURNKEY_API_BASE_URL || '',
-  apiPrivateKey: process.env.TURNKEY_API_PRIVATE_KEY || '',
-  apiPublicKey: process.env.TURNKEY_API_PUBLIC_KEY || '',
-  defaultOrganizationId: process.env.NEXT_PUBLIC_TURNKEY_ORGANIZATION_ID || '',
+const turnkeyApi = new Turnkey({
+  apiBaseUrl: process.env.NEXT_PUBLIC_TURNKEY_API_BASE_URL!,
+  apiPrivateKey: process.env.NEXT_PUBLIC_TURNKEY_API_PRIVATE_KEY!,
+  apiPublicKey: process.env.NEXT_PUBLIC_TURNKEY_API_PUBLIC_KEY!,
+  defaultOrganizationId: process.env.NEXT_PUBLIC_TURNKEY_ORGANIZATION_ID!,
 });
 
-export const turnkeyApiClient = turnkey.apiClient();
+const turnkeyBrowser = new PasskeyClient({
+  apiBaseUrl: process.env.NEXT_PUBLIC_TURNKEY_API_BASE_URL!,
+  defaultOrganizationId: process.env.NEXT_PUBLIC_TURNKEY_ORGANIZATION_ID!,
+  rpId: 'localhost',
+});
+
+export const turnkeyApiClient = turnkeyApi.apiClient();
+export const turnkeyPasskeyClient = turnkeyBrowser.passkeyClient();
