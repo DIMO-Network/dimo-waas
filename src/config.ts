5f5db54a2d4ba144a74cf6905454a89fd8a240e0
Crystal Adkins <CrystalAdkins09@gmail.com>
2024-09-10 11:12:13 -0400

    Still trying to get this to deploy

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,7 @@ import {cookieStorage, createConfig} from '@alchemy/aa-alchemy/config';
 import {polygonAmoy} from '@alchemy/aa-core';
 import {QueryClient} from '@tanstack/react-query';
 
-// @ts-ignore
-export const chain = polygonAmoy;
+export const chain: any = polygonAmoy;
 // TODO look into how to set up this alchemy config to use the Turnkey signer
 export const config = createConfig({
   // this is for requests to the specific chain RPC
